d78f41780a5ae9689a7aa8bd443901071e504387
Abdullah Tanveer <abdullahkhan1430789@gmail.com>
2024-10-26 10:54:01 +0500

    First push

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,56 @@
-
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+const cors=require("cors");//different URL pr route krny mey help krta hai
+const connectDB=require("./Work/DB/database.js");
+
+
+// import and configuration of dotenv
+const dotenv =require("dotenv");
+dotenv.config();
+
+// const user_router=require("./Practice/Routes/userRoute");
+
+
+
+
+
+
+
+
+app.use(express.json());// Middleware to parse JSON data
+app.use(cors());
+
+
+// Routes
+// app.use("/user",authRouter);
+// app.use(productRouter);
+
+// const user_router=require("./All SIMs details/Routes/userRoutes.js");
+// const admin_router=require("./All SIMs details/Routes/adminRoutes.js");
+
+// //to use router
+// app.use(user_router);   
+// app.use(admin_router);   
+
+// app.get('/insert', (req, res) => {
+//   console.log("Shukr hai");
+//   res.send("Route hit: Shukr hai");
+// });
+
+
+
+
+
+
+
+//PORT
+const port = process.env.PORT || 5000;  
+
+//connection of mongoDB
+connectDB();
+
+app.listen(port, () => {
+  console.log(`Server run at ${port}`);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
